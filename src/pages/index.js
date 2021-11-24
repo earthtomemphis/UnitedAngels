459a435e0daa27eb21d21c747b8ecfbfab8d56c3
Memphis DiAngelis <earthtomemphis@gmail.com>
2021-11-23 22:31:26 -0700

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className="container text-center justify-center align-items-center flex flex-col m-auto w-full">
+      <h1 className="text-center text-6xl mt-4 text-yellow-500 m-auto">United Angels</h1>
+      <iframe className="justify-center align-items-center m-auto mt-2" width="560" height="315" src="https://www.youtube.com/embed/V_ori8CUkIc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <h2 className="text-4xl mt-4">Welcome to the United Angels home page!!!</h2>
+  </div>
+  )
 }
